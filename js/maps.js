6102c6716d5b4a23252225cfc23e83d24021cc3b
schneidan <dan@scrollwright.com>
2015-11-11 16:31:40 -0700

    Maps segments all updated with notes. Notes restored.

--- js/maps.js
+++ js/maps.js
@@ -15,7 +15,7 @@ var mapchunks = {
 	1: {
 		name: 'CT Segment 1',
 		desc: 'Kassler to South Platte Canyon &mdash; 16.8 miles',
-		notes: 'The Colorado Trail&rsquo;s northern trailhead will be crowded with hikers and cyclists, but the crowds fade (and big horn sheep might appear) as the trail follows the river through the canyon. ',
+		notes: 'The Colorado Trail&rsquo;s northern trailhead at Waterton Canyon will be crowded with hikers and cyclists as it travels on a mostly flat road paralleling the South Platte River through the canyon.',
 		lat: '39.449150',
 		lon: '-105.138734',
 		zoom: 11,
@@ -28,7 +28,7 @@ var mapchunks = {
 	2: {
 		name: 'CT Segment 2',
 		desc: 'South Platte Canyon to Colorado Trailhead FS 550 &mdash; 11.5 miles',
-		notes: 'Some of this segment passes through the old Hayman Fire burn, so there&rsquo;s not much shade, nor water. The stark beauty of the burn grows into timber near Forest Service Road 550.',
+		notes: 'The majority of this segment passes through the old Buffalo Creek Burn. There&rsquo;s not much shade, nor water, but there is relief in pine forests at either end of the segment. Water is available from a spigot at the Elk Creek fire station.',
 		lat: '39.376788',
 		lon: '-105.215726',
 		zoom: 11,
@@ -41,7 +41,7 @@ var mapchunks = {
 	3: {
 		name: 'CT Segment 3',
 		desc: 'Colorado Trailhead FS 550 to FS 560 (Wellington Lake Road) &mdash; 12.2 miles',
-		notes: 'Here the trail rolls through ponderosa pine and aspen groves. This segment is popular with mountain bikers and trail runners.',
+		notes: 'Here the trail rolls through ponderosa pine and aspen groves, and many small streams, on wonderful singletrack. Expect lots of company from mountain bikers, hikers and trail runners.',
 		lat: '39.336438',
 		lon: '-105.321126',
 		zoom: 11,
@@ -54,7 +54,7 @@ var mapchunks = {
 	4: {
 		name: 'CT Segment 4',
 		desc: 'FS 560 (Wellington Lake Road) to Long Gulch &mdash; 16.6 miles',
-		notes: 'This quiet section takes a moderate climb through forest. Part of the segment is in the Lost Creek Wilderness.',
+		notes: 'This quiet segment enters the Lost Creek Wilderness. Lost Creek runs through a six-mile-long meadow &mdash; aptly named Long Gulch &mdash; rimmed by pine forest.',
 		lat: '39.337500',
 		lon: '-105.506864',
 		zoom: 11,
@@ -67,7 +67,7 @@ var mapchunks = {
 	5: {
 		name: 'CT Segment 5',
 		desc: 'Long Gulch to Kenosha Pass &mdash; 15.1 miles',
-		notes: 'A long meadow and a low saddle &mdash; Kenosha Pass &mdash; dominate this segment of the hike.',
+		notes: 'In the open country in this segment, near Kenosha Pass, between stretches of aspen grove and forest, you&rsquo;ll catch views of South Park and some of the nearby peaks.',
 		lat: '39.390322',
 		lon: '-105.690192',
 		zoom: 11,
@@ -80,7 +80,7 @@ var mapchunks = {
 	6: {
 		name: 'CT Segment 6',
 		desc: 'Kenosha Pass to Goldhill Trailhead &mdash; 32.9 miles',
-		notes: 'Between the stretches of aspen grove and forest, you&rsquo;ll catch open views into South Park and some of the nearby peaks. The pyramid of Mount Guyot towers over Georgia Pass on this segment.',
+		notes: 'You&rsquo;ll find nice singletrack, lush forests and great meadows for camping between Kenosha Pass and Breckenridge. The horizon on Georgia Pass is dominated by pyramid-shaped Mount Guyot.',
 		lat: '39.479417',
 		lon: '-105.897565',
 		zoom: 11,
@@ -93,7 +93,7 @@ var mapchunks = {
 	7: {
 		name: 'CT Segment 7',
 		desc: 'Goldhill Trailhead to Copper Mountain &mdash; 12.8 miles',
-		notes: 'This high, exposed section of trail crosses the Tenmile Range before dropping down to Copper Mountain.',
+		notes: 'High country, spectacular vistas. The trail crosses a ridge at 12,500 feet between Peaks 5 and 6 in the Tenmile Range. There&rsquo;s water near Breckenridge and Copper Mountain, but not much in between.',
 		lat: '39.523127',
 		lon: '-106.108365',
 		zoom: 11,
@@ -106,7 +106,7 @@ var mapchunks = {
 	8: {
 		name: 'CT Segment 8',
 		desc: 'Copper Mountain to Tennessee Pass &mdash; 25.4 miles',
-		notes: 'Departing Copper under the chair lifts, the trail climbs primo singletrack to Searle and Kokomo passes. At U.S. 24, near Ski Cooper, there&rsquo;s a hiker box on the trail near the rest area.',
+		notes: 'This segment passes through Copper Mountain ski resort on nice singletrack and climbs Searle and Kokomo passes, both well above treeline. Janet&rsquo;s Cabin, available by reservation, sits just below Searle Pass. Camp Hale, the training site for the famous 10th Mountain Division, isn&rsquo;t far from Tennessee Pass.',
 		lat: '39.431171',
 		lon: '-106.234364',
 		zoom: 11,
@@ -119,7 +119,7 @@ var mapchunks = {
 	9: {
 		name: 'CT Segment 9',
 		desc: 'Tennessee Pass to Hagerman Pass Road &mdash; 13.6 miles',
-		notes: 'In this segment, the trail enters the Holy Cross Wilderness, where there&rsquo;s plentiful water in rushing streams &hellip; and then a long climb over a ridge before the trail drops to Hagerman Pass Road.',
+		notes: 'The highlight of this segment is the great mountain scenery in the Holy Cross Wilderness. There&rsquo;s good camping in meadows and near streams and small lakes throughout, with one exception: Don&rsquo;t rely on Wurts Ditch, near Tennessee Pass, for water.',
 		lat: '39.319707',
 		lon: '-106.382680',
 		zoom: 11,
@@ -132,7 +132,7 @@ var mapchunks = {
 	10: {
 		name: 'CT Segment 10',
 		desc: 'Hagerman Pass Road to Halfmoon Creek &mdash; 13 miles',
-		notes: 'Here the trail is a beautiful, rocky walk along a roaring stream that heads down almost to Turquoise Lake. You&rsquo;ll have to walk out to the road from the Timberline Lake trailhead to see it, though.',
+		notes: 'Expect lots of day hikers in the Mt. Massive Wilderness. This is the main route to climb Mt. Massive, 14,421 feet. Because you spend most of your time in deep forest, many hikers call this the &ldquo;green tunnel.&rdquo;',
 		lat: '39.213652',
 		lon: '-106.418385',
 		zoom: 11,
@@ -145,7 +145,7 @@ var mapchunks = {
 	11: {
 		name: 'CT Segment 11',
 		desc: 'Half Moon Creek to Clear Creek Road &mdash; 21.5 miles',
-		notes: 'This segment heads deep in the woods of the Mount Massive Wilderness, then to Twin Lakes. Here, hikers have to decide whether to stick to the tried-and-true Colorado Trail on the Collegiate East route or the new Collegiate West trail, which takes a detour (you guessed it) west at Twin Lakes.',
+		notes: 'More of the &ldquo;green tunnel,&rdquo; but along with the pine forest there are huge, beautiful aspen groves. You can divert to climb Colorado&rsquo;s highest peak, Mt. Elbert, in this segment. The Collegiate East and Collegiate West routes intersect at the tiny village of Twin Lakes.',
 		lat: '39.085321',
 		lon: '-106.344914',
 		zoom: 11,
@@ -158,7 +158,7 @@ var mapchunks = {
 	12: {
 		name: 'CT Segment 12',
 		desc: 'Clear Creek Road to North Cottonwood Creek &mdash; 18.5 miles',
-		notes: 'There&rsquo;s good camping &mdash; and plenty of up and down &mdash; here in the Collegiate Peaks, plus an option for a big, rugged detour to Missouri Basin. ',
+		notes: 'Lots of up and down in this segment, including over the ridge of Waverly Mountain and down to beautiful streams like Pine Creek and Frenchman&rsquo;s Creek. Good camping around Harvard Lakes. At Pine Creek there&rsquo;s an option for a big, rugged detour to Missouri Basin.',
 		lat: '38.946343',
 		lon: '-106.260800',
 		zoom: 11,
@@ -171,7 +171,7 @@ var mapchunks = {
 	13: {
 		name: 'CT Segment 13',
 		desc: 'North Cottonwood Creek Road to Chalk Creek &mdash; 22.8 miles',
-		notes: 'Pass through here at the right time of year and you&rsquo;ll be greeted by meadows of wildflowers. The trail passes close, very close, to Mt. Princeton Hot Springs.',
+		notes: 'You&rsquo;ll have a good climb up to the shoulder of Mt. Yale, no matter which direction you come from, and an option to climb the peak itself. There are restrooms at the Avalanche Trailhead. The trail runs close to Mount Princeton Hot Springs. The resort has great ice cream at the store and will accept hiker boxes.',
 		lat: '38.792644',
 		lon: '-106.240888',
 		zoom: 11,
@@ -184,7 +184,7 @@ var mapchunks = {
 	14: {
 		name: 'CT Segment 14',
 		desc: 'Chalk Creek Trailhead to U.S. 50 &mdash; 20.4 miles',
-		notes: 'The trail rolls through forest, meadows and streams here before descending to U.S. 50, not far from Salida.',
+		notes: 'This segment of the trail rolls along through beautiful aspen and pine forest &mdash; especially stunning in the autumn. From Monarch Pass, it&rsquo;s an easy hitch to Salida for rest, relaxation and resupply. The Monarch Crest Store on top of the pass has an awesome hiker box and serves up a mean chili dog.',
 		lat: '38.631103',
 		lon: '-106.234021',
 		zoom: 11,
@@ -197,7 +197,7 @@ var mapchunks = {
 	15: {
 		name: 'CT Segment 15',
 		desc: 'U.S. 50 to Marshall Pass &mdash; 14.3 miles',
-		notes: 'There are spectacular views of the Sawatch Mountains along here, and a high passage along a ridge. The section from here to Spring Creek Pass is the longest section of trail without a resupply.',
+		notes: 'A walk along the Continental Divide with spectacular views in all directions. There&rsquo;s much exposure to weather here, and once you leave U.S. 50 there are no real resupply points for 80 miles. Gunnison and Creede are the closest towns, and neither is within easy reach.',
 		lat: '38.476992',
 		lon: '-106.272473',
 		zoom: 11,
@@ -210,7 +210,7 @@ var mapchunks = {
 	16: {
 		name: 'CT Segment 16',
 		desc: 'Marshall Pass to Sargents Mesa &mdash; 15.2 miles',
-		notes: 'More of the same, and few sources of water through this remote section of trail.',
+		notes: 'The trail passes through meadow and forest, with big mountain views in the distance. Tank Seven Creek at the foot of Sargents Mesa are just about the only reliable water sources in this segment &mdash; though if you keep your eyes peeled, there are occasional small springs, seeps and stock tanks.',
 		lat: '38.340326',
 		lon: '-106.328092',
 		zoom: 11,
@@ -223,7 +223,7 @@ var mapchunks = {
 	17: {
 		name: 'CT Segment 17',
 		desc: 'Sargents Mesa to Colorado 114 &mdash; 20.4 miles',
-		notes: 'And still more high ranching country, still with few water sources.',
+		notes: 'Sargents Mesa is beautiful: high, wide, lonesome and mostly waterless. The forest, when you&rsquo;re in it, is filled with deadfall, wildflowers and berries of all flavors. Reliable water is available in tiny Razor Creek and at Baldy Lake.',
 		lat: '38.278635',
 		lon: '-106.484990',
 		zoom: 11,
@@ -236,7 +236,7 @@ var mapchunks = {
 	18: {
 		name: 'CT Segment 18',
 		desc: 'Colorado 114 to Saguache Park Road &mdash; 13.8 miles',
-		notes: 'The trail often follows dirt roads through open meadows in this stretch, but the lack of water is finally relieved at Cochetopa Creek.',
+		notes: 'A high prairie walk on a mix of trail and rutted four-wheel-drive ranch roads through sagebrush, grass and pasture land. Expect cows! This section can often be hot, windy and exposed to the weather. Lujan Creek and Los Creek have water.',
 		lat: '38.181411',
 		lon: '-106.638455',
 		zoom: 11,
@@ -249,7 +249,7 @@ var mapchunks = {
 	19: {
 		name: 'CT Segment 19',
 		desc: 'Saguache Park Road to Eddiesville Trailhead &mdash; 13.7 miles',
-		notes: 'There are more open roads to follow through the Cochetopa Hills in this segment. The trail climbs steadily to enter the La Garita Wilderness here.',
+		notes: 'More cows! More open country! Water is available in springs, one of them fenced off but accessible. Gradually the timber that has been only on the ridgelines creeps lower until you&rsquo;re walking through aspen and pines. You&rsquo;ll have to ford Cochetopa Creek, a major crossing in early season when the water&rsquo;s high.',
 		lat: '38.075680',
 		lon: '-106.775098',
 		zoom: 11,
@@ -262,7 +262,7 @@ var mapchunks = {
 	20: {
 		name: 'CT Segment 20',
 		desc: 'Eddiesville Trailhead to San Luis Pass &mdash; 12.7 miles',
-		notes: 'Any boredom hikers feel in the previous few segments is alleviated in the La Garita Wilderness and the climb to San Luis Pass. The wild creeks, forests and mountains here are some of the most beautiful on the trail.',
+		notes: 'The remote La Garita Wilderness is spectacular, even though hundreds of square miles of the forest have been devastated by pine beetle kill. Water is plentiful. San Luis Peak, at 14,014 feet, is accessible by a hike from the trail.',
 		lat: '37.998344',
 		lon: '-106.894231',
 		zoom: 11,
@@ -275,7 +275,7 @@ var mapchunks = {
 	21: {
 		name: 'CT Segment 21',
 		desc: 'San Luis Pass to Spring Creek Pass &mdash; 14.9 miles',
-		notes: 'The trail goes through high, exposed country here, up to 12,917 feet, and passes Snow Mesa, an expanse entirely above treeline.',
+		notes: 'Although there are brief sections with trees for shelter, the trail between San Luis Pass and Spring Creek Pass is high and exposed. It travels the Continental Divide at an average elevation above 12,000 feet. Snow Mesa, a broad, high, flat expanse, makes for a cautious crossing.',
 		lat: '37.965059',
 		lon: '-107.060056',
 		zoom: 11,
@@ -288,7 +288,7 @@ var mapchunks = {
 	22: {
 		name: 'CT Segment 22',
 		desc: 'Spring Creek Pass to Carson Saddle &mdash; 17.2 miles',
-		notes: 'After crossing Colorado 149, the route crosses the highest portions of trail, yielding big views of endless mountains.',
+		notes: 'In this segment you&rsquo;ll climb to Coney Summit, 13,271 feet, the highpoint of The Colorado Trail. The tiny alpine wildflowers are outrageously beautiful along this section. Because it&rsquo;s so high, water isn&rsquo;t readily available except in snowfields, from snowmelt ponds and tiny trickles of melt.',
 		lat: '37.899798',
 		lon: '-107.267766',
 		zoom: 11,
@@ -301,7 +301,7 @@ var mapchunks = {
 	23: {
 		name: 'CT Segment 23',
 		desc: 'Carson Saddle to Rio Grande Reservoir Road &mdash; 15.9 miles',
-		notes: 'A crossing of the Rio Grande River is involved here.',
+		notes: 'This is still high-altitude, remote country with little in the way of cover, outside of pockets of willows, should a storm blow in. Many hikers camp at Cataract Lake. Many moose!',
 		lat: '37.834208',
 		lon: '-107.459340',
 		zoom: 11,
@@ -314,7 +314,7 @@ var mapchunks = {
 	24: {
 		name: 'CT Segment 24',
 		desc: 'Rio Grande Reservoir Road to Molas Pass &mdash; 20.2 miles',
-		notes: '',
+		notes: 'The trail between Stony Pass and Molas Pass is nothing short of spectacular &mdash; Colorado&rsquo;s wild mountain country at its finest. The tundra crossing is high and exposed, but it&rsquo;s also filled with vast fields of wildflowers. The Weminuche Wilderness is another awe-inspiring walk with water, water everywhere. This segment is epic.',
 		lat: '37.750103',
 		lon: '-107.614865',
 		zoom: 11,
@@ -327,7 +327,7 @@ var mapchunks = {
 	25: {
 		name: 'CT Segment 25',
 		desc: 'Molas Pass to Bolam Pass Road &mdash; 20.9 miles',
-		notes: '',
+		notes: 'Between Molas Pass to Bolam Pass Road, the trail crosses fields of flowers framed by stunning views of the San Juan Mountains.',
 		lat: '37.736801',
 		lon: '-107.808843',
 		zoom: 11,
@@ -340,7 +340,7 @@ var mapchunks = {
 	26: {
 		name: 'CT Segment 26',
 		desc: 'Bolam Pass Road to Hotel Draw Road &mdash; 10.9 miles',
-		notes: '',
+		notes: 'More high country and more climbing and descending as you make your way over Blackhawk Pass. Water should be plentiful here, but it&rsquo;s not. Straight Creek is the most reliable source.',
 		lat: '37.682207',
 		lon: '-107.954926',
 		zoom: 11,
@@ -353,7 +353,7 @@ var mapchunks = {
 	27: {
 		name: 'CT Segment 27',
 		desc: 'Hotel Draw Road to Cumberland Basin &mdash; 20.6 miles',
-		notes: '',
+		notes: 'The trail crosses and utilizes a number of logging roads; it&rsquo;s not well marked. There&rsquo;s little water; some is available off trail via short, steep hikes to springs. Indian Trail Ridge, a narrow, above-treeline walk between the San Juan Mountains and the La Plata range, is a storm magnet.',
 		lat: '37.539694',
 		lon: '-108.001103',
 		zoom: 11,
@@ -366,7 +366,7 @@ var mapchunks = {
 	28: {
 		name: 'CT Segment 28',
 		desc: 'Kennebec Trailhead to Junction Creek Trailhead &mdash; 21.5 miles',
-		notes: '',
+		notes: 'The stretches of the trail near Durango are crowded with day hikers, runners and cyclists in Canyon Creek, but the rest of the segment, on either side of Kennebec Pass, the trail is high, lush and quiet.',
 		lat: '37.388725',
 		lon: '-107.957501',
 		zoom: 11,
@@ -379,7 +379,7 @@ var mapchunks = {
 	29: {
 		name: 'CW Segment 1',
 		desc: 'Twin Lakes to Sheep Gulch &mdash; 9.8 miles',
-		notes: '',
+		notes: 'This segment of the trail crosses Hope Pass, 12,548 feet. The trail is steep but the views on top are worth the climb.',
 		lat: '39.042936',
 		lon: '-106.382680',
 		zoom: 11,
@@ -392,7 +392,7 @@ var mapchunks = {
 	30: {
 		name: 'CW Segment 2',
 		desc: 'Sheep Gulch to Cottonwood Pass Road Trailhead &mdash; 25.2 miles',
-		notes: '',
+		notes: 'Cross another high pass, Lake Ann Pass, at 12,588 feet, and enjoy some of the most beautiful country on the trail with views of the Three Apostles and Ice Mountain. This segment is a mix of high tundra country and low, lush forest and meadow, all carpeted with wildflowers.',
 		lat: '38.916698',
 		lon: '-106.452374',
 		zoom: 11,
@@ -405,7 +405,7 @@ var mapchunks = {
 	31: {
 		name: 'CW Segment 3',
 		desc: 'Cottonwood Pass Road Trailhead to Garden Basin Trailhead &mdash; 10.2 miles',
-		notes: '',
+		notes: 'With the exception of Prospector Gulch and Texas Creek, this is all high country, rolling along between 10,000 and 12,000 feet with the majority on the higher end. Amazing views in all directions. There&rsquo;s much climbing and descending.',
 		lat: '38.793713',
 		lon: '-106.436925',
 		zoom: 11,
@@ -418,7 +418,7 @@ var mapchunks = {
 	32: {
 		name: 'CW Segment 4',
 		desc: 'Garden Basin Trailhead to Hancock Trailhead &mdash; 14.2 miles',
-		notes: '',
+		notes: 'Lots more climbing and descending, with every ridge top revealing a new drainage, each more beautiful than the one preceding it. Walking on the old railroad grade of the Alpine Tunnel provides the easiest and lowest hiking on the Collegiate West.',
 		lat: '38.694101',
 		lon: '-106.413235',
 		zoom: 11,
@@ -431,7 +431,7 @@ var mapchunks = {
 	33: {
 		name: 'CW Segment 5',
 		desc: 'Hancock Trailhead to Ridge Above South Fooses Creek &mdash; 22.6 miles',
-		notes: '',
+		notes: 'High lakes are a highlight in this segment: Hancock Lake, Trail Lake, Boss Lake. Be sure to look around at the ancient ruins of rock walls and hunting pits along the Continental Divide on Monarch Ridge.',
 		lat: '38.547107',
 		lon: '-106.343197',
 		zoom: 11,
@@ -472,6 +472,8 @@ function setMapLinks(seg) {
 	var prev = (prev == -1) ? 33 : prev;
 	var pageCurr = 'li#seg' + seg;
 
+	var notesBuilt = ( segment['notes'].length > 5 ) ? '<strong>Dean&rsquo;s notes:</strong> ' + segment['notes'] : '';
+
 	$('a#mapNext').attr('href','javascript:mapSeg(' + next + ')');
 	$('a#mapPrev').attr('href','javascript:mapSeg(' + prev + ')');
 	$('a#segPagNext').attr('href','javascript:mapSeg(' + next + ')');
@@ -480,7 +482,7 @@ function setMapLinks(seg) {
 	$(pageCurr).addClass('current');
 	$('#segTitle').html(segment['name']);
 	$('#segDesc').html(segment['desc']);
-	//$('#Dnotes').html(segment['notes']);
+	$('#Dnotes').html(notesBuilt);
 	$('#elevpng').attr('src','./kml/' + segment['elimg']);
 	$('#elmin').html('<strong>Min:</strong> ' + segment['elmin'] + ' feet');
 	$('#elmax').html('<strong>Max:</strong> ' + segment['elmax'] + ' feet');
